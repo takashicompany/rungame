805db6981593af0cc4c97996275535c281dbbbde
takashicompany <t@kashi.company>
2022-01-12 12:38:50 +0900

    DepthStackker

---
+++ Scripts/DepthStackker.cs
@@ -0,0 +1,12 @@
+namespace takashicompany.RunGame
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Linq;
+	using UnityEngine;
+
+	public class DepthStackker : MonoBehaviour
+	{
+		
+	}
+}

a324d1317d0b42866f1a822b0cd0b63f4034a331
takashicompany <t@kashi.company>
2022-01-18 12:26:31 +0900

    Hopperを作成

---
+++ Scripts/Hopper.cs
@@ -0,0 +1,38 @@
+namespace takashicompany.RunGame
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Linq;
+	using UnityEngine;
+
+	[RequireComponent(typeof(Rigidbody))]
+	public class Hopper : Runner
+	{
+		[SerializeField]
+		private Vector3 _hopPosition;
+
+		[SerializeField]
+		private Vector3 _hopPower;
+
+		private void Hop(Vector3 power, Vector3 position)
+		{
+			_rigidbody.AddForceAtPosition(power, position);
+		}
+
+		public void Hop()
+		{
+			Hop(_hopPower, _hopPosition);
+		}
+
+		private Vector3 GetHopWorldPosition()
+		{
+			return transform.TransformVector(_hopPosition);
+		}
+
+		private void OnDrawGizmos()
+		{
+			Gizmos.color = Color.red;
+			takashicompany.Unity.Utils.DrawGizmosPoint(GetHopWorldPosition());
+		}
+	}
+}

67c94ccd5bb92dd7115cd403ca4864d4dd56788e
takashicompany <t@kashi.company>
2022-01-18 12:47:39 +0900

    アクセスできるMainPlayerを追加

---
+++ Scripts/MainPlayer.cs
@@ -0,0 +1,23 @@
+namespace takashicompany.RunGame
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Linq;
+	using UnityEngine;
+	using takashicompany.Unity;
+
+	public class MainPlayer : TaBehabviour
+	{
+		private Runner _runnerIntenral;
+
+		protected Runner runner => _runnerIntenral ?? (_runnerIntenral = GetComponent<Runner>());
+
+		private PickUpper _pickUpperInternal;
+
+		protected PickUpper _pickUpper => _pickUpperInternal ?? (_pickUpperInternal = GetComponent<PickUpper>());
+
+		private Hopper _hopperInternal;
+
+		protected Hopper _hopper => _hopperInternal ?? (_hopperInternal = GetComponent<Hopper>());
+	}
+}
